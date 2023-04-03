b15e28f0e18317acc25979be55b99a98001ad00b
Prashant Mishra <prashantmishra111@outlook.com>
2023-04-03 06:02:27 +0530

    !!

--- index.js
+++ index.js
@@ -1,25 +1,4 @@
-// const express = require('express')
-// const path= require('path')
-// const app = express()
-// const PORT = process.env.PORT || 3000
-
-// app.set('view engine', 'ejs')
-
-// app.set('views', path.join(__dirname, 'views'));
-// app.use(express.static(__dirname + '/public'));
-
-// app.get('/', (req, res) => {
-//   res.render('index')
-// })
-
-// app.get('/about', (req, res) => {
-//   res.render('about')
-// })
-
-// app.listen(PORT, () => {
-//   console.log(`Example app listening on port ${PORT}`)
-// })
-
+ 
 // Imports--
 const express = require("express");
 
--- models/accessModel.js
+++ models/accessModel.js
@@ -1,15 +1,15 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
 
-const accessSchema = new Schema({
-  sessionId: {
-    type: String,
-    require: true,
-  },
-  time: {
-    type: String,
-    require: true,
-  },
-});
+// const accessSchema = new Schema({
+//   sessionId: {
+//     type: String,
+//     require: true,
+//   },
+//   time: {
+//     type: String,
+//     require: true,
+//   },
+// });
 
-module.exports = mongoose.model("access", accessSchema);
+// module.exports = mongoose.model("access", accessSchema);
